2ae197e4a433a0864d00a73148358a1df2da08b6
Marcus ParkerGarruba <mparkergarruba@gmail.com>
2022-08-10 12:35:45 -0400

    commit tree 3b175424ded67d040fe58926ad1e67c538417281

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,6 @@
 class AuthorsController < ApplicationController
+
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   
   def show
     author = Author.find(params[:id])
@@ -7,15 +9,19 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
+    author = Author.create!(author_params)
 
     render json: author, status: :created
   end
 
   private
-  
-  def author_params
-    params.permit(:email, :name)
-  end
+
+    def render_unprocessable_entity_response(ent)
+      render json: { errors: ent.record.errors }, status: :unprocessable_entity
+    end
+
+    def author_params
+      params.permit(:email, :name)
+    end
   
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
 
   def show
     post = Post.find(params[:id])
@@ -9,15 +11,19 @@ class PostsController < ApplicationController
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
+    post.update!(post_params)
 
     render json: post
   end
 
   private
 
-  def post_params
-    params.permit(:category, :content, :title)
-  end
+    def render_unprocessable_entity_response(ent)
+      render json: { errors: ent.record.errors }, status: :unprocessable_entity
+    end
+
+    def post_params
+      params.permit(:category, :content, :title)
+    end
 
 end
